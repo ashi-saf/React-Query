8326c813ae39e44396fd87acb186108ca58e9efb
ashi-saf <152131562+ashi-saf@users.noreply.github.com>
2024-09-17 21:51:26 +0530

    Update http.js

--- frontend/src/utils/http.js
+++ frontend/src/utils/http.js
@@ -27,7 +27,7 @@ export async function fetchEvents({ signal, searchTerm, max }) {
 }
 
 export async function createNewData(eventData) {
-  const response = await fetch('http://localhost:3000/events', {
+  const response = await fetch('https://events-backend-8ju1.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -46,7 +46,7 @@ export async function createNewData(eventData) {
 }
 
 export async function fetchSelectableImages({ signal }) {
-  const response = await fetch('http://localhost:3000/events/images', {
+  const response = await fetch('https://events-backend-8ju1.onrender.com/images', {
     signal: signal,
   });
   if (!response.ok) {
@@ -61,7 +61,7 @@ export async function fetchSelectableImages({ signal }) {
 }
 
 export async function fetchEvent({ signal, id }) {
-  const response = await fetch(`http://localhost:3000/events/${id}`, {
+  const response = await fetch(`https://events-backend-8ju1.onrender.com/${id}`, {
     signal: signal,
   });
   if (!response.ok) {
@@ -75,7 +75,7 @@ export async function fetchEvent({ signal, id }) {
   return event;
 }
 export async function deleteEvent({ id }) {
-  const response = await fetch(`http://localhost:3000/events/${id}`, {
+  const response = await fetch(`https://events-backend-8ju1.onrender.com/${id}`, {
     method: 'DELETE',
   });
 
@@ -90,7 +90,7 @@ export async function deleteEvent({ id }) {
 }
 
 export async function updateData({ id, event }) {
-  const response = await fetch(`http://localhost:3000/events/${id}`, {
+  const response = await fetch(`https://events-backend-8ju1.onrender.com/${id}`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
