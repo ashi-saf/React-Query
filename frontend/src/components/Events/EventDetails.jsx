0da4c203017952d0169d4fb0c70723028e1a65eb
ashi-saf <152131562+ashi-saf@users.noreply.github.com>
2024-09-18 12:24:55 +0530

    Update EventDetails.jsx

--- frontend/src/components/Events/EventDetails.jsx
+++ frontend/src/components/Events/EventDetails.jsx
@@ -82,7 +82,7 @@ export default function EventDetails() {
           </nav>
         </header>
         <div id="event-details-content">
-          <img src={`http://localhost:3000/${data.image}`} alt={data.title} />
+          <img src={`https://events-frontend-b7fl.onrender.com/${data.image}`} alt={data.title} />
           <div id="event-details-info">
             <div>
               <p id="event-details-location">{data.location}</p>
